b9285a862f2f789c714e396ef5a549d8df74a0d4
ShakalakaB <yunjie989@gmail.com>
2021-05-13 17:12:40 +0800

    Add getter and setter

--- src/main/java/aldora/spring/recipe/model/Recipe.java
+++ src/main/java/aldora/spring/recipe/model/Recipe.java
@@ -108,4 +108,20 @@ public class Recipe {
     public void setNotes(Notes notes) {
         this.notes = notes;
     }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 }
