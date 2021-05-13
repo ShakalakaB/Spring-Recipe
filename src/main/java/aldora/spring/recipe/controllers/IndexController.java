05f4f64b085d007c82acea5934d3b30e5ee4a793
ShakalakaB <yunjie989@gmail.com>
2021-05-13 20:50:29 +0800

    Add query methods in repository

--- src/main/java/aldora/spring/recipe/controllers/IndexController.java
+++ src/main/java/aldora/spring/recipe/controllers/IndexController.java
@@ -1,13 +1,31 @@
 package aldora.spring.recipe.controllers;
 
+import aldora.spring.recipe.model.Category;
+import aldora.spring.recipe.model.UnitOfMeasure;
+import aldora.spring.recipe.repositories.CategoryRepository;
+import aldora.spring.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println(categoryOptional.get().getId());
+        System.out.println(unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/aldora/spring/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/aldora/spring/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package aldora.spring.recipe.repositories;
 import aldora.spring.recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
