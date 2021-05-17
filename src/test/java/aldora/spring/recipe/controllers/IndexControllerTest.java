453bd48adf74ba376d6a26607871c80f807385d3
ShakalakaB <yunjie989@gmail.com>
2021-05-17 12:36:50 +0800

    Add ArgumentCaptor in IndexControllerTest

--- src/test/java/aldora/spring/recipe/controllers/IndexControllerTest.java
+++ src/test/java/aldora/spring/recipe/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import aldora.spring.recipe.model.Recipe;
 import aldora.spring.recipe.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -31,10 +32,25 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        Set<Recipe> recipesSet = new HashSet<>();
+        recipesSet.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipesSet.add(recipe);
+
+        ArgumentCaptor<Set<Recipe>> recipeCaptor = ArgumentCaptor.forClass(Set.class);
+
+        when(recipeService.getRecipes()).thenReturn(recipesSet);
+
         String result = indexController.getIndexPage(model);
 
         assertEquals("index", result);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), recipeCaptor.capture());
+
+        Set<Recipe> recipesCaptureValue = recipeCaptor.getValue();
+        assertEquals(2, recipesCaptureValue.size());
     }
 }
