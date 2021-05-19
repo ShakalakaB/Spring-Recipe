67d5fd11a2b8a980e76400e686c8c9797a836282
ShakalakaB <yunjie989@gmail.com>
2021-05-19 15:27:32 +0800

    Create recipe

--- src/main/java/aldora/spring/recipe/controllers/RecipeController.java
+++ src/main/java/aldora/spring/recipe/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package aldora.spring.recipe.controllers;
 
+import aldora.spring.recipe.commands.RecipeCommand;
 import aldora.spring.recipe.model.Recipe;
 import aldora.spring.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -21,5 +21,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipe);
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }
