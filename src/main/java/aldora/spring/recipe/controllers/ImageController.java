590b67566d2b72daec8da5d3cf531c9a56c9b6b1
ShakalakaB <yunjie989@gmail.com>
2021-05-21 16:55:22 +0800

    Display image

---
+++ src/main/java/aldora/spring/recipe/controllers/ImageController.java
@@ -0,0 +1,62 @@
+package aldora.spring.recipe.controllers;
+
+import aldora.spring.recipe.commands.RecipeCommand;
+import aldora.spring.recipe.model.Recipe;
+import aldora.spring.recipe.services.ImageService;
+import aldora.spring.recipe.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Controller
+public class ImageController {
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public ImageController(ImageService imageService, RecipeService recipeService) {
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{recipeId}/image")
+    public String showUploadForm(@PathVariable String recipeId, Model model) {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(Long.valueOf(recipeId));
+
+        model.addAttribute("recipe", recipeCommand);
+
+        return "recipe/imageUploadForm";
+    }
+
+    @PostMapping("/recipe/{recipeId}/image")
+    public String handleImagePost(@PathVariable String recipeId, @RequestParam("imagefile") MultipartFile file) {
+        imageService.saveImageFile(Long.valueOf(recipeId), file);
+
+        return "redirect:/recipe/" + recipeId + "/show";
+    }
+
+    @GetMapping("/recipe/{recipeId}/recipeimage")
+    public void showImage(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+        Recipe recipe = recipeService.findById(Long.valueOf(recipeId));
+
+        if (recipe.getImage() != null) {
+            byte[] byteObject = new byte[recipe.getImage().length];
+            int i = 0;
+
+            for (byte b : recipe.getImage()) {
+                byteObject[i++] = b;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream inputStream = new ByteArrayInputStream(byteObject);
+            IOUtils.copy(inputStream, response.getOutputStream());
+        }
+    }
+}
---
+++ src/test/java/aldora/spring/recipe/controllers/imageControllerTest.java
@@ -0,0 +1,64 @@
+package aldora.spring.recipe.controllers;
+
+import aldora.spring.recipe.model.Recipe;
+import aldora.spring.recipe.services.ImageService;
+import aldora.spring.recipe.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class ImageControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    ImageService imageService;
+
+    ImageController imageController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        imageController = new ImageController(imageService, recipeService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+    }
+
+    @Test
+    void ShowImage() throws Exception {
+        String s = "fake image text";
+        Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+        int i = 0;
+
+        for (byte primByte : s.getBytes()){
+            bytesBoxed[i++] = primByte;
+        }
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipe.setImage(bytesBoxed);
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+        assertEquals(s.getBytes().length, responseBytes.length);
+
+    }
+}
