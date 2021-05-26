4d869a9269b3f649fa8a00ad9e3f70763ff6a6a3
ShakalakaB <yunjie989@gmail.com>
2021-05-26 23:29:48 +0800

    Edit circle ci config

--- src/main/java/aldora/spring/recipe/commands/IngredientCommand.java
+++ src/main/java/aldora/spring/recipe/commands/IngredientCommand.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class IngredientCommand {
     private String id;
-    private Long recipeId;
+    private String recipeId;
     private String description;
     private BigDecimal amount;
     private UnitOfMeasureCommand unitOfMeasure;
