1f822427c093274675f03df4e1d0bc05beca7fcb
ShakalakaB <yunjie989@gmail.com>
2021-05-28 15:36:40 +0800

    Fix mongo integration test

--- src/test/java/aldora/spring/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/aldora/spring/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,10 +1,13 @@
 package aldora.spring.recipe.repositories;
 
+import aldora.spring.recipe.bootstrap.RecipeBootStrap;
 import aldora.spring.recipe.model.UnitOfMeasure;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,13 +16,29 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-@Ignore
+
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        recipeRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        RecipeBootStrap recipeBootStrap = new RecipeBootStrap(recipeRepository, unitOfMeasureRepository, categoryRepository);
+        recipeBootStrap.onApplicationEvent(null);
+    }
 
     @Test
     public void findByDescription() {
