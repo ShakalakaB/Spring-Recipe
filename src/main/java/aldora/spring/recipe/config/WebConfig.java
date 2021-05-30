5100344627434308b3eb8444b70dbf3ba7de2500
ShakalakaB <yunjie989@gmail.com>
2021-05-31 01:40:05 +0800

    Intro router functions

---
+++ src/main/java/aldora/spring/recipe/config/WebConfig.java
@@ -0,0 +1,26 @@
+package aldora.spring.recipe.config;
+
+import aldora.spring.recipe.model.Recipe;
+import aldora.spring.recipe.services.RecipeService;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route((GET("/api/recipes")),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(recipeService.getRecipes(), Recipe.class)
+        );
+    }
+}
