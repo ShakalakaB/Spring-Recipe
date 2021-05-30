60746f70e32b98a7dac1a5fb8db1a01e14a7b20c
ShakalakaB <yunjie989@gmail.com>
2021-05-31 01:03:31 +0800

    Add exception handler

--- src/main/java/aldora/spring/recipe/controllers/ControllerExceptionHandler.java
+++ src/main/java/aldora/spring/recipe/controllers/ControllerExceptionHandler.java
@@ -1,31 +1,35 @@
 package aldora.spring.recipe.controllers;
 
+import aldora.spring.recipe.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception) {
-//        log.error("Handling not found exception");
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("errors/404");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
-//
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleRuntimeException(Exception exception) {
-//        log.error("Handling bad request exception");
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("errors/404");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public String handleNotFound(Exception exception, Model model) {
+        log.error("Handling not found exception");
+
+        model.addAttribute("exception", exception);
+
+        return "errors/404";
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({NumberFormatException.class, WebExchangeBindException.class})
+    public String handleRuntimeException(Exception exception, Model model) {
+        log.error("Handling bad request exception");
+        log.error("excepiton: ", exception);
+
+        model.addAttribute("exception", exception);
+
+        return "errors/400";
+    }
 }
