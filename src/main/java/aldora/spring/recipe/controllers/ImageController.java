6832f0ed0513b6df3dc9818c46e13cf599c52092
ShakalakaB <yunjie989@gmail.com>
2021-05-30 15:57:27 +0800

    Convert to webflux

--- src/main/java/aldora/spring/recipe/controllers/ImageController.java
+++ src/main/java/aldora/spring/recipe/controllers/ImageController.java
@@ -4,13 +4,11 @@ import aldora.spring.recipe.commands.RecipeCommand;
 import aldora.spring.recipe.model.Recipe;
 import aldora.spring.recipe.services.ImageService;
 import aldora.spring.recipe.services.RecipeService;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,21 +40,21 @@ public class ImageController {
         return "redirect:/recipe/" + recipeId + "/show";
     }
 
-    @GetMapping("/recipe/{recipeId}/recipeimage")
-    public void showImage(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
-        Recipe recipe = recipeService.findById(recipeId).block();
-
-        if (recipe.getImage() != null) {
-            byte[] byteObject = new byte[recipe.getImage().length];
-            int i = 0;
-
-            for (byte b : recipe.getImage()) {
-                byteObject[i++] = b;
-            }
-
-            response.setContentType("image/jpeg");
-            InputStream inputStream = new ByteArrayInputStream(byteObject);
-            IOUtils.copy(inputStream, response.getOutputStream());
-        }
-    }
+//    @GetMapping("/recipe/{recipeId}/recipeimage")
+//    public void showImage(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+//        Recipe recipe = recipeService.findById(recipeId).block();
+//
+//        if (recipe.getImage() != null) {
+//            byte[] byteObject = new byte[recipe.getImage().length];
+//            int i = 0;
+//
+//            for (byte b : recipe.getImage()) {
+//                byteObject[i++] = b;
+//            }
+//
+//            response.setContentType("image/jpeg");
+//            InputStream inputStream = new ByteArrayInputStream(byteObject);
+//            IOUtils.copy(inputStream, response.getOutputStream());
+//        }
+//    }
 }
